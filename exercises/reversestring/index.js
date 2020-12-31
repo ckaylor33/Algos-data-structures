c73bd38d55750817809b8be3f11476932286da19
ckaylor33 <kaylor33@hotmail.co.uk>
2020-12-31 20:03:16 +0000

    two solutions for reversing a string

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// SOLUTION
+function reverse(str) {
+  let reversed = "";
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+}
+
+reverse("apple");
+module.exports = reverse;
+
+// SOLUTION
+// function reverse(str) {
+//     return str.split("").reverse().join("");
+//   }
