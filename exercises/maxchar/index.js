e4b394b31f4bc2d3eb555856936db13da5d6b2a1
ckaylor33 <kaylor33@hotmail.co.uk>
2021-01-11 09:20:12 +0000

    alternate solutions

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,31 +5,61 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-// SOLUTION 2 - forEach
+// SOLUTION 4
 function maxChar(str) {
-  const charMap = {}
-  let max = 0
+  const chars = {}
   let maxChar = ''
+  let maxValue = 0
 
-  let charArr = str.split('')
-
-  charArr.forEach((char) => {
-    if (!charMap[char]) {
-      charMap[char] = 1
-    } else {
-      charMap[char]++
-    }
-  })
-  for (let char in charMap) {
-    if (charMap[char] > max) {
-      max = charMap[char]
-      maxChar = char
-    }
+  for (let char of str) {
+    chars[char] = chars[char] + 1 || 1
   }
+  maxValue = Math.max(...Object.values(chars))
+  maxChar = Object.keys(chars).find((key) => chars[key] === maxValue)
+
   return maxChar
 }
 
 module.exports = maxChar
+
+// SOLUTION 3 - Object.keys
+// function maxChar(str) {
+//   const ht = {}
+
+//   for (let char of str) {
+//     if (!ht[char]) {
+//       ht[char] = 1
+//     } else {
+//       ht[char]++
+//     }
+//   }
+
+//   return Object.keys(ht).reduce((a, b) => (ht[a] > ht[b] ? a : b))
+// }
+
+// SOLUTION 2 - forEach
+// function maxChar(str) {
+//   const charMap = {}
+//   let max = 0
+//   let maxChar = ''
+
+//   let charArr = str.split('')
+
+//   charArr.forEach((char) => {
+//     if (!charMap[char]) {
+//       charMap[char] = 1
+//     } else {
+//       charMap[char]++
+//     }
+//   })
+//   for (let char in charMap) {
+//     if (charMap[char] > max) {
+//       max = charMap[char]
+//       maxChar = char
+//     }
+//   }
+//   return maxChar
+// }
 
 // for of loop? forEach?
 // if char isn't there assign value
